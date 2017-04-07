33bf7f03a99d54b37d6804f0195c69efb5063287
Cole Allan <cacoleallan@gmail.com>
2017-04-06 20:00:08 -0600

    adding styles to main view

--- src/app/Fancy.js
+++ src/app/Fancy.js
@@ -2,14 +2,17 @@ import React, { Component } from 'react';
 import {
     StyleSheet,
     View,
-    Text
+    Text,
+    Dimensions
 } from 'react-native';
 
+const {width, height} = Dimensions.get('window');
 export class Fancy extends Component {
     render() {
         return (
             <View style={styles.container}>
-                <Text>Fancy</Text>
+                <View style={styles.box}></View>
+                <Text style={styles.text}>Fancy</Text>
             </View>
         )
     }
@@ -18,6 +21,23 @@ export class Fancy extends Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: `grey`
+        backgroundColor: `grey`,
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 50
+    },
+    box: {
+        backgroundColor: 'yellow',
+        width: width / 2,
+        height: height / 3,
+        position: 'absolute',
+        top: 20,
+        left: 30,
+        borderRadius: 30
+    },
+    text: {
+        color: 'white',
+        fontSize: 34,
+        fontWeight: 'bold'
     }
 });
